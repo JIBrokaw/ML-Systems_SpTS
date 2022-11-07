c1015abdccd48871e6234ae7acbff94ea8c82921
Jeannine Brokaw <49129316+JIBrokaw@users.noreply.github.com>
2022-11-06 22:11:53 -0500

    Add files via upload
    Added timer and usage of GPU

--- matrix_mult.cu
+++ matrix_mult.cu
@@ -7,10 +7,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <cuda_runtime.h>
+#include <sys/time.h>
+#include "common_string.h"
 
 
-// __global__
-void basic_mult(float* matrix_A, float* matrix_B, float* out, int size)
+
+__global__ void basic_mult(float* matrix_A, float* matrix_B, float* out, int size)
 {
   for (int output_col = 0; output_col < size; output_col++){
     for (int row = 0; row < size; row++){
@@ -54,16 +56,7 @@ bool multiply(int matrix_size, float sparsity)
             h_B[i*matrix_size + j] = rand()/(float)RAND_MAX < sparsity ? rand() / (float)RAND_MAX : 0;
         }
     }
-
-    basic_mult(h_A, h_B, h_out, matrix_size);
-
-    printf("Matrix A:\n ");
-    printMatrix(h_A, matrix_size);
-    printf("Matrix B:\n ");
-    printMatrix(h_B, matrix_size);
-    printf("Result:\n ");
-    printMatrix(h_out, matrix_size);
-
+    printf("Matrices initialized!\n");
 
     // allocate device memory
     cudaMalloc((void**)&d_A, bytes);
@@ -72,84 +65,47 @@ bool multiply(int matrix_size, float sparsity)
     cudaMemcpy(d_A, h_A, bytes, cudaMemcpyHostToDevice);
     cudaMemcpy(d_B, h_B, bytes, cudaMemcpyHostToDevice);
 
-    // launch Kernel
-    // printf("\n %s\n", "Launch Kernel....");
-    // Timer timer;
-    // if (kernel_type == "vanilla") {
-    //     // basic 2d conv
-    //     //todo: define block and grid size
-    //     dim3 dimBlock(16, 16, 1);
-	  //     dim3 dimGrid(1, 1, 1);
-    //     printf("block dim: %d x %d x %d\n", dimBlock.x, dimBlock.y, dimBlock.z);
-    //     printf("grid dim: %d x %d x %d\n", dimGrid.x, dimGrid.y, dimGrid.z);
-    //
-    //     //todo: warmup
-    //
-    //     startTime(&timer);
-    //     //todo: launch kernel on device and test performance, get results
-    //     convolution2D<<<dimBlock, dimGrid>>>(d_in, d_out, width, height, channels, kernel_width);
-	  //     cudaDeviceSynchronize();
-    //     stopTime(&timer);
-    //     double flopsPerMatrixMul = 2.0 * static_cast<double>(kernel_width) * \
-    //                                 static_cast<double>(kernel_width) * static_cast<double>(kernel_width); \
-    //     double numMatrixMul = width*height*channels;
-    //     throughput = (numMatrixMul*flopsPerMatrixMul * 1.0e-9f) / (elapsedTime(timer) / 1000.0f);
-    // }
-    // else if (kernel_type == "shared_mem") {
-    //     //  2d conv on shared mem
-    //     //todo: define block and grid size
-    //
-    //     dim3 dimBlock(16, 16, 1);
-    //     dim3 dimGrid(16, 16, 1);
-    //
-    //     printf("block dim: %d x %d x %d\n", dimBlock.x, dimBlock.y, dimBlock.z);
-    //     printf("grid dim: %d x %d x %d\n", dimGrid.x, dimGrid.y, dimGrid.z);
-    //
-    //     //todo: warmup
-    //     convolution2D_sharedmem<<<dimBlock, dimGrid>>>(d_in, d_out, width, height, channels, kernel_width);
-    //
-    //     startTime(&timer);
-    //     convolution2D_sharedmem<<<dimBlock, dimGrid>>>(d_in, d_out, width, height, channels, kernel_width);
-    //     cudaDeviceSynchronize();
-    //     stopTime(&timer);
-    //
-    //     double flopsPerMatrixMul = 2.0 * static_cast<double>(kernel_width) * \
-    //                                 static_cast<double>(kernel_width) * static_cast<double>(kernel_width); \
-    //     double numMatrixMul = width*height*channels;
-    //     throughput = (numMatrixMul*flopsPerMatrixMul * 1.0e-9f) / (elapsedTime(timer) / 1000.0f);
-    // }
-    // todo: bonus optimization
-    /*
-    else if (kernel_type == "optimized") {
-    }
-    */
+    printf("\n %s\n", "Launch Kernel....");
 
-    // result in CPU
-    // float* cpu_out = (float*)malloc(bytes);
-    // printf("\nCalculating in CPU...\n");
-    // verify(h_in, cpu_out, h_kernel, width, height, channels, kernel_width);
-    //
-    // int precision = 8;
-    // double threshold = 1e-8 * channels*width*height;
-    // double diff = 0.0;
-    //
-    // //todo: compare kernel result with CPU result
-    // float* gpu_out = (float*)malloc(bytes);
-    // CUDA_CHECK(cudaMemcpy(gpu_out, d_out, bytes, cudaMemcpyDeviceToHost));
-    // printf("%s\n", cudaGetErrorString(cudaGetLastError()));
-    //
-    // for(int i = 0; i < bytes/sizeof(float); i++){
-    //   diff += abs(cpu_out[i] - gpu_out[i]);
+    struct timeval startTime;
+    struct timeval endTime;
+
+    dim3 dimBlock(16, 16, 1);
+    dim3 dimGrid(1, 1, 1);
+    printf("block dim: %d x %d x %d\n", dimBlock.x, dimBlock.y, dimBlock.z);
+    printf("grid dim: %d x %d x %d\n", dimGrid.x, dimGrid.y, dimGrid.z);
+    printf("Matrix size: %d*%d\n", matrix_size, matrix_size);
+
+    //     //todo: warmup
+
+    gettimeofday(&startTime, NULL);
+    basic_mult<<<dimBlock, dimGrid>>>(d_A, d_B, d_out, matrix_size);
+    cudaDeviceSynchronize();
+    gettimeofday(&endTime, NULL);
+    printf("Time elapsed: %f seconds", (float) ((endTime.tv_sec - startTime.tv_sec)  + (endTime.tv_usec - startTime.tv_usec)/1.0e6));
+    //     double flopsPerMatrixMul = 2.0 * static_cast<double>(kernel_width) * \
+    //                                 static_cast<double>(kernel_width) * static_cast<double>(kernel_width); \
+    //     double numMatrixMul = width*height*channels;
+    //     throughput = (numMatrixMul*flopsPerMatrixMul * 1.0e-9f) / (elapsedTime(timer) / 1000.0f);
     // }
-    //
-    // printf("Kernel width is %d \n", kernel_width);
-    //
+
+    float* gpu_out = (float*)malloc(bytes);
+    cudaMemcpy(gpu_out, d_out, bytes, cudaMemcpyDeviceToHost);
+
+    if(matrix_size <= 15){
+      printf("\nMatrix A:\n ");
+      printMatrix(h_A, matrix_size);
+      printf("Matrix B:\n ");
+      printMatrix(h_B, matrix_size);
+      printf("Result:\n ");
+      printMatrix(gpu_out, matrix_size);
+    }
+
     // //todo: getting result
     // printf("[Kernel %s] Throughput = %.4f GB/s, Time = %.5f ms\n",
     //     kernel_type, throughput, elapsedTime(timer)*1000);
     // printf("Error : %.*f (threshold: %f)\n", precision, (double)diff, threshold);
 
-    // todo:
     // free memory (both device and host mem)
 
     cudaFree(d_A);
@@ -159,7 +115,7 @@ bool multiply(int matrix_size, float sparsity)
     free(h_B);
     free(h_out);
     // free(cpu_out);
-    // free(gpu_out);
+    free(gpu_out);
 
     return true;
 }
