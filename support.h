4da896026dc20ce86d13c2209ee9c89ed4869240
Jeannine Brokaw <49129316+JIBrokaw@users.noreply.github.com>
2022-11-14 13:44:50 -0500

    New function header for creating csr from scratch

--- support.h
+++ support.h
@@ -15,6 +15,7 @@ void printMatrix(float* matrix, int matrix_size);
 void printCsr(CSR* matrix, int matrix_size);
 void printVector(float* vector, int size);
 //Converts a sparse triangular matrix in an array form to csr format
+void create_random_csr(CSR* csr, int size, float sparsity);
 void convert_matrix_to_csr(CSR* csr, float* matrix, int size);
 #ifdef __cplusplus
 }
